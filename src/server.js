2675beed093b6b7d9d606af45d2b80e22410a853
OLuciani <lucianioscar1@gmail.com>
2024-03-22 22:13:30 -0300

    Arrglos en server.js

--- src/server.js
+++ src/server.js
@@ -22,30 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-// Configuración del middleware CORS
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "http://localhost:8081" // Reemplaza esto con la URL de tu frontend
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    res.setHeader("Access-Control-Max-Age", 7200); // Duración máxima de la solicitud preflight en segundos
-  
-    next();
-  });
-
-
-
-//app.use(cors());
+app.use(cors());
 app.use(methodOverride('_method'));
 
 
