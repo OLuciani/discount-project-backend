d6af229897cd0dc2f0b022ef2a7302a86107b690
OLuciani <lucianioscar1@gmail.com>
2024-12-11 23:45:51 -0300

    Cambio los roles permitidos en la ruta /userDiscountQrs_update/:_id

--- src/routes/userDiscountQrsRoute.js
+++ src/routes/userDiscountQrsRoute.js
@@ -8,6 +8,9 @@ import userDiscountQrsController from "../controllers/userDiscountQrsController.
 
 //const roleAdminWeb = process.env.ROLE_ADMINWEB;
 const roleAdminQr = process.env.ROLE_ADMINQR;
+const roleBusinessDirector = process.env.ROLE_BUSINESS_DIRECTOR;
+const roleBusinessManager = process.env.ROLE_BUSINESS_MANAGER;
+const roleBusinessEmployee = process.env.ROLE_BUSINESS_EMPLOYEE;
 
 
 router.get("/userDiscountQrs_list", userDiscountQrsController.userDiscountQrs_list);
@@ -15,7 +18,7 @@ router.get("/userDiscountQrs_list", userDiscountQrsController.userDiscountQrs_li
 router.get("/userDiscountQrs_OneUser", authenticateToken, userDiscountQrsController.userDiscountQrs_OneUser);
 router.post("/discountQr_create", authenticateToken, userDiscountQrsController.discountQr_create);
 router.get("/userDiscountQrs_oneDiscount/:_id", authenticateToken, userDiscountQrsController.userDiscountQrs_oneDiscount);
-router.patch("/userDiscountQrs_update/:_id", authenticateToken, authorizeRole([roleAdminQr]), userDiscountQrsController.userDiscountQrs_update);
+router.patch("/userDiscountQrs_update/:_id", authenticateToken, authorizeRole([roleBusinessEmployee, roleBusinessManager, roleBusinessDirector]), userDiscountQrsController.userDiscountQrs_update);
 router.get("/consumed_discounts", authenticateToken, userDiscountQrsController.consumed_discounts);
 
 export default router;
