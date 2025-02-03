e3b2d4f5e55475a0422bd8209ccd95465c4c8334
OLuciani <lucianioscar1@gmail.com>
2025-02-02 23:59:57 -0300

    Modifico cookie en el archivo src/controllers/usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -840,7 +840,7 @@ const controller = {
         sameSite: 'Lax',
         maxAge: 15 * 60 * 1000, // 15 minutos
         path: '/', // Accesible en toda la app
-        domain: 'discount-project-web.vercel.app',
+        //domain: 'discount-project-web.vercel.app',
       });
   
       res.json({
